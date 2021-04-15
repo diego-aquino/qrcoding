b1593a7cdb2e44fa0fd9aad19f461a1291944097
Diego Aquino <diego.cruz001@gmail.com>
2021-04-15 11:04:26 -0300

    Initialize project and set up dev environment

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+
+class Document extends NextDocument {
+  render(): JSX.Element {
+    return (
+      <Html lang="pt-BR">
+        <Head />
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default Document;
