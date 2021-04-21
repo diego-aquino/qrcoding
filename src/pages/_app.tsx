efe5e03f01b7bc58a2d2af203fbc766f107f2a95
Diego Aquino <diego.cruz001@gmail.com>
2021-04-21 17:55:43 -0300

    Add support to SASS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app';
 import { FC } from 'react';
 
-import '~/styles/globals.css';
+import '~/styles/globals.scss';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => (
   <Component {...pageProps} />
