9ba316aa6e081a56bf999f67d053d62fa1d83a50
Julio Hsu <tnt700900@gmail.com>
2021-04-30 20:49:53 -0300

    download

---
+++ src/@types/saveSvgAsPng.d.ts
@@ -0,0 +1,4 @@
+declare module 'save-svg-as-png' {
+  // eslint-disable-next-line no-unused-vars
+  export const saveSvgAsPng: (svgElement: Node, fileName: string) => void;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import QRCode from 'qrcode.react';
 import { ChangeEvent, FC, useCallback, useState } from 'react';
+import { saveSvgAsPng } from 'save-svg-as-png';
 
-import { CircularRightArrow, CloudDownloadFrame, CopyFrame } from '~/assets';
-import { Layout } from '~/components/common';
+import { CircularRightArrow, CloudDownloadFrame } from '~/assets';
+import { Layout, Button } from '~/components/common';
 import styles from '~/styles/pages/HomePage.module.scss';
-
-import Button from '../components/common/Button';
 
 // 2953 characters, using binary enconding and error correction level "L"
 const MAX_QRCODE_CHARACTER_LENGTH = 2953;
@@ -25,6 +24,13 @@ const HomePage: FC = () => {
     [],
   );
 
+  function downloadQrcode() {
+    const svg = document.getElementById('qrcode-svg');
+    if (svg != null) {
+      saveSvgAsPng(svg, 'qrcode');
+    }
+  }
+
   return (
     <Layout pageTitle="QR Coding" className={styles.container}>
       <h1>Compartilhe sua mensagem...</h1>
@@ -36,18 +42,22 @@ const HomePage: FC = () => {
           placeholder="Digite o seu texto aqui..."
         />
         <CircularRightArrow />
-        <QRCode className={styles.qrCode} value={qrCodeValue} renderAs="svg" />
+        <QRCode
+          className={styles.qrCode}
+          value={qrCodeValue}
+          renderAs="svg"
+          id="qrcode-svg"
+        />
       </div>
 
       <div className={styles.downloadContainer}>
-        <Button variant="primary" className={styles.downloader}>
+        <Button
+          variant="primary"
+          className={styles.downloader}
+          onClick={downloadQrcode}
+        >
           Download
           <CloudDownloadFrame />
-        </Button>
-
-        <Button variant="secondary" className={styles.copier}>
-          Copiar
-          <CopyFrame />
         </Button>
       </div>
     </Layout>
